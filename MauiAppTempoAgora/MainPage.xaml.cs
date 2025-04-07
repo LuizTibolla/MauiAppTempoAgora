da6723de0d859dfd364dfade8142d6f97320eb55
luizt <luizt@LUIZTIBOLLA>
2025-04-06 21:45:29 -0300

    Atualizaçao 1.0

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.Threading.Tasks;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -7,18 +11,59 @@
         public MainPage()
         {
             InitializeComponent();
+            VerificarConexao();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async Task VerificarConexao()
         {
-            count++;
+            var acesso = Connectivity.Current.NetworkAccess;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            if(acesso != NetworkAccess.Internet)
+            {
+                await DisplayAlert("Erro | Sem conexão com a internet", "Por favor Verifique sua conexão a Internet e tente novamente", "OK");
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private async void btnBuscar(object sender, EventArgs e)
+        {
+            try
+            {
+                if(!string.IsNullOrEmpty(txtCidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txtCidade.Text);
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Cidade: {txtCidade.Text}\n" +
+                                        $"Temperatura: {t.temp_max}°C\n" +
+                                        $"Temperatura Mínima: {t.temp_min}°C\n" +
+                                        $"Descrição: {t.description}\n" +
+                                        $"Velocidade do Vento: {t.speed} m/s\n" +
+                                        $"Visibilidade: {t.visibility} m\n" +
+                                        $"Latitude: {t.lat}\n" +
+                                        $"Longitude: {t.lon}\n" +
+                                        $"Nascer do Sol: {t.sunrise}\n" +
+                                        $"Por do Sol: {t.sunset}";
+
+                        txtlblResultado.Text = dados_previsao;
+                    }
+                    else 
+                    {
+                        
+                    }
+
+                }
+                else
+                {
+                    txtlblResultado.Text = "Digite uma cidade";
+                }
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
         }
     }
 
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,51 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "5da2ae238d5e9d564a27b618759fb3a3";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave}&units=metric&lang=pt_br";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime sunrise = DateTimeOffset.FromUnixTimeSeconds((int)rascunho["sys"]["sunrise"]).DateTime;
+                    DateTime sunset = DateTimeOffset.FromUnixTimeSeconds((int)rascunho["sys"]["sunset"]).DateTime;
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = (int)rascunho["sys"]["sunrise"],
+                        sunset = (int)rascunho["sys"]["sunset"]
+                    };
+                }
+                else
+                {
+                    throw new Exception("Cidade não encontrada");
+                }
+            }
+            return t;
+        }
+    }
+}
